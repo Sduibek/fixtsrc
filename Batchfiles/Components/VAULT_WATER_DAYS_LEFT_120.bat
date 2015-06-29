2507a48f8978b64896ab5d8ce2e572108fffe22c
Sduibek <falloutfixt@gmail.com>
2015-06-28 21:35:51 -0700

    Order change to "Days Left" batch files

--- Batchfiles/Components/VAULT_WATER_DAYS_LEFT_120.bat
+++ Batchfiles/Components/VAULT_WATER_DAYS_LEFT_120.bat
@@ -1,5 +1,5 @@
-sed.exe s/"VAULT_WATER               :=64000"/"VAULT_WATER               :=120"/ VAULT13.GAM > globalvars.tmp
-cmd.exe /c copy globalvars.tmp VAULT13.GAM /Y
 sed.exe s/"VAULT_WATER               :=150"/"VAULT_WATER               :=120"/ VAULT13.GAM > globalvars.tmp
+cmd.exe /c copy globalvars.tmp VAULT13.GAM /Y
+sed.exe s/"VAULT_WATER               :=64000"/"VAULT_WATER               :=120"/ VAULT13.GAM > globalvars.tmp
 cmd.exe /c copy globalvars.tmp VAULT13.GAM /Y
 echo VAULT_WATER_120 >> ..\DATA\FIXT\install.log
